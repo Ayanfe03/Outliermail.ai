0aef80cf668f872113dd06c33689d8f708574e88
Oluwasuan Ayanfeoluwa <ayanfe1711@gmail.com>
2025-06-16 23:53:19 +0100

    Added aurinko authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import LinkAccountButton from '@/components/link-account-button';
 import { Button } from '@/components/ui/button'
-export default async function Home() {
-  return <Button>Hello World!</Button>;
+export default async function Home() { 
+  return <LinkAccountButton />
 }
+
+
 
 
