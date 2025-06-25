144cd3da5d27f74acd1edc73b006cd8860eca9f1
Oluwasuan Ayanfeoluwa <ayanfe1711@gmail.com>
2025-06-25 20:32:24 +0100

    added page for mail dashboard

---
+++ src/app/mail/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MailDashboard = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-2xl font-bold">Mail Dashboard</h1>
+    </div>
+  );
+}
+
+export default MailDashboard;
